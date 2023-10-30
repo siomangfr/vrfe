c37954b6b6b3f253e5ab697e4775fa42704d10a3
siomangfr <149304008+siomangfr@users.noreply.github.com>
2023-10-30 18:26:46 +0700

    Update punakawan.sh

--- punakawan.sh
+++ punakawan.sh
@@ -52,7 +52,6 @@ sleep 120
 echo lagi ngeloni
 sleep 120
 screen -X -S "prabowo1" quit
-cd prabowo1
 sqd down
 cd ..
 echo Dapet cewek 2 cindo sugeng tapi tepos ... gass reddorz
@@ -103,7 +102,6 @@ sleep 120
 echo lagi ngeloni
 sleep 120
 screen -X -S "prabowo2" quit
-cd prabowo2
 sqd down
 cd ..
 echo Dapet cewek 3 medan semok tapi kurang good looking ... gass reddorz
@@ -154,7 +152,6 @@ sleep 120
 echo lagi ngeloni
 sleep 120
 screen -X -S "prabowo3" quit
-cd prabowo3
 sqd down
 cd ..
 echo Dapet cewek 4 bandung semok tapi agak item ... gass reddorz
@@ -205,7 +202,6 @@ sleep 120
 echo lagi ngeloni
 sleep 120
 screen -X -S "prabowo4" quit
-cd prabowo4
 sqd down
 cd ..
 echo Dapet cewek 5 cindo spek bidadari ... gass reddorz ... ampe pagi ini mah
@@ -306,6 +302,5 @@ sleep 120
 echo lagi ngeloni
 sleep 120
 screen -X -S "prabowo5" quit
-cd prabowo5
 sqd down
 echo Akhirnya CROOOOT juga
