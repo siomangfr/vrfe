c5d52ac109da878ca985a2bd79e6ff699c938b74
siomangfr <149304008+siomangfr@users.noreply.github.com>
2023-10-30 18:23:02 +0700

    Create punakawan.sh

---
+++ punakawan.sh
@@ -0,0 +1,311 @@
+echo Lagi cari cewek MiChat ... Sabar
+sqd init prabowo1 -t https://github.com/siomangfr/single-chain-squid &> /dev/null
+sqd init prabowo2 -t https://github.com/siomangfr/double-chain-squid &> /dev/null
+sqd init prabowo3 -t https://github.com/siomangfr/triple-chain-squid &> /dev/null
+sqd init prabowo4 -t https://github.com/siomangfr/quad-chain-squid &> /dev/null
+sqd init prabowo5 -t https://github.com/siomangfr/snapshot-squid &> /dev/null
+echo Dapet cewek 1 cindo tapi susu kecil ... gass reddorz
+cd prabowo1
+sqd up &> /dev/null
+npm ci &> /dev/null
+sqd build &> /dev/null
+sqd migration:apply &> /dev/null 
+screen -dmS prabowo1 sqd run .
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+screen -X -S "prabowo1" quit
+cd prabowo1
+sqd down
+cd ..
+echo Dapet cewek 2 cindo sugeng tapi tepos ... gass reddorz
+cd prabowo2
+sqd up &> /dev/null
+npm ci &> /dev/null
+sqd build &> /dev/null
+sqd migration:apply &> /dev/null 
+screen -dmS prabowo2 sqd run .
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+screen -X -S "prabowo2" quit
+cd prabowo2
+sqd down
+cd ..
+echo Dapet cewek 3 medan semok tapi kurang good looking ... gass reddorz
+cd prabowo3
+sqd up &> /dev/null
+npm ci &> /dev/null
+sqd build &> /dev/null
+sqd migration:apply &> /dev/null 
+screen -dmS prabowo3 sqd run .
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+screen -X -S "prabowo3" quit
+cd prabowo3
+sqd down
+cd ..
+echo Dapet cewek 4 bandung semok tapi agak item ... gass reddorz
+cd prabowo4
+sqd up &> /dev/null
+npm ci &> /dev/null
+sqd build &> /dev/null
+sqd migration:apply &> /dev/null 
+screen -dmS prabowo4 sqd run .
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+screen -X -S "prabowo4" quit
+cd prabowo4
+sqd down
+cd ..
+echo Dapet cewek 5 cindo spek bidadari ... gass reddorz ... ampe pagi ini mah
+cd prabowo5
+sqd up &> /dev/null
+npm ci &> /dev/null
+sqd build &> /dev/null
+sqd migration:apply &> /dev/null 
+screen -dmS prabowo5 sqd run .
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+echo lagi ngeloni
+sleep 120
+screen -X -S "prabowo5" quit
+cd prabowo5
+sqd down
+echo Akhirnya CROOOOT juga
